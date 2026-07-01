f9ea89ae3c55878088349bdf1824c6879460011d
egbert <shixiaofei04@meituan.com>
2026-07-01 14:30:02 +0800

    Initial Commit

---
+++ NatureVoice/NatureVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatureVoiceApp.swift
+//  NatureVoice
+//
+//  Created by egbert on 2026/7/1.
+//
+
+import SwiftUI
+
+@main
+struct NatureVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
